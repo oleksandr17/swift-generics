a11c99251ce8ae0cbe0ff25252841d0f9d1c8bf0
Oleksandr <oleksandr@elements.nl>
2019-03-09 11:51:24 +0100

    Add Generic Types

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -7,14 +7,32 @@ func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
     b = temporaryA
 }
 
-var a = 0
-var b = 9
-swapTwoValues(&a, &b)
-print(a)
-print(b)
+var someInt = 3
+var anotherInt = 107
+swapTwoValues(&someInt, &anotherInt)
+print(someInt, anotherInt)
 
-var c = "hello"
-var d = "world"
-swapTwoValues(&c, &d)
-print(c)
-print(d)
+var someString = "hello"
+var anotherString = "world"
+swapTwoValues(&someString, &anotherString)
+print(someString, anotherString)
+
+//
+// Generic Types
+
+struct Stack<Element> {
+    private var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+let fromTheTop = stackOfStrings.pop()
