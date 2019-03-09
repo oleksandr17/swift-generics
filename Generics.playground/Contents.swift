250e66e7d3287c3bffe21bcd850370e5b258475f
Oleksandr <oleksandr@elements.nl>
2019-03-09 13:06:53 +0100

    Add Type Constraints

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -47,3 +47,18 @@ extension Stack {
 }
 
 print(String(describing: stackOfStrings.topItem))
+
+//
+// Type Constraints
+
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
